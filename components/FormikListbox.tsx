c30130e454c26f5ddadabf62a8a5f5ce8df3f169
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-12-27 22:07:38 +0800

    Adding some styling

--- components/FormikListbox.tsx
+++ components/FormikListbox.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { Listbox } from "@headlessui/react";
+import cn from "classnames";
 
 export type ListboxOption<T> = {
   label: string;
@@ -17,19 +18,48 @@ type Props<T> = {
 
 const FormikListbox = <T,>(props: Props<T>) => {
   const options = props.options;
-  const [selected, setSelected] = useState<T | undefined>(props?.value);
+  const [selected, setSelected] = useState<T | undefined>();
 
-  const _selected = options.find((o) => o.value === props.value);
+  const _selected = options.find((o) => o.value === selected);
+  const _label = _selected?.label ?? props.label ?? "Select Option...";
+
   return (
-    <Listbox value={selected} onChange={setSelected}>
-      <Listbox.Button>{_selected?.label ?? "Select Option..."}</Listbox.Button>
-      <Listbox.Options>
-        {options.map((option, i) => (
-          <Listbox.Option key={i} value={option.value}>
-            {option.label}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
+    <Listbox value={selected} onChange={setSelected} as={React.Fragment}>
+      {({ open }) => (
+        <div
+          className={cn({
+            "dropdown dropdown-end w-full": true,
+            "dropdown-open": open,
+          })}
+        >
+          <Listbox.Button className="btn btn-outline w-full relative no-animation">
+            {_label}
+          </Listbox.Button>
+          <Listbox.Options
+            className={cn({
+              "dropdown-content menu p-2 shadow-lg": true,
+              "bg-base-100 rounded-box w-72": true,
+              hidden: !open,
+            })}
+          >
+            {options.map((option, i) => (
+              <Listbox.Option key={i} value={option.value}>
+                {({ active, disabled, selected }) => (
+                  <button
+                    className={cn({
+                      active: selected,
+                      "btn-disabled": disabled,
+                      "bg-primary/80 text-primary-content": active,
+                    })}
+                  >
+                    {option.label}
+                  </button>
+                )}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </div>
+      )}
     </Listbox>
   );
 };
